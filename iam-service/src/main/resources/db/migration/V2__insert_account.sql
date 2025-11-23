fb81e83ac6a0a333e554946d8e2f39ce1f6f5aac
darrenak403 <ngothanhdat4002@gmail.com>
2025-11-23 16:42:23 +0700

    Refactor application configuration and update database connection settings

--- iam-service/src/main/resources/db/migration/V2__insert_account.sql
+++ iam-service/src/main/resources/db/migration/V2__insert_account.sql
@@ -1,22 +1,3 @@
-INSERT INTO role (role_name, role_code, description, privileges)
-VALUES ('Administrator', 'ROLE_ADMIN', 'System administrator with full access to all system features.', 'FULL_ACCESS');
-
-INSERT INTO role (role_name, role_code, description, privileges)
-VALUES ('Laboratory Manager', 'ROLE_MANAGER', 'Responsible for managing the lab, lab users, service users, and monitoring the overall system.', 'MANAGE_LAB');
-
-INSERT INTO role (role_name, role_code, description, privileges)
-VALUES ('Service', 'ROLE_STAFF', 'Authorized personnel for system operation and maintenance, ensuring optimal performance and reliability.', 'SYSTEM_MAINTENANCE');
-
-INSERT INTO role (role_name, role_code, description, privileges)
-VALUES ('Doctor', 'ROLE_DOCTOR', 'Laboratory staff responsible for conducting tests, analyzing samples, and managing lab processes.', 'LAB_OPERATIONS');
-
-INSERT INTO role (role_name, role_code, description, privileges)
-VALUES ('Patient', 'ROLE_PATIENT', 'Patient user with permission to view their own test results only.', 'READ_ONLY');
-
-
--- User
-INSERT INTO user (email, password, role_id)
-VALUES ('admin@admin.com', '$2a$12$P.xnkEV7SRbCblNUNaSdVeTtBJovmHVHI2tCQvdF.UXvYjIJ5T5FO', '1');
 
 INSERT INTO user (email, password, role_id)
 VALUES ('staff@staff.com', '$2a$12$7a1lC0FPGbKtEUHSHGsoKuAeVIe2bz4I5y3C0BxfxVOEAi8CQvdHe', '3');
